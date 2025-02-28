b9381ff72055ad527c3315ecb3be320a4949bd4d
nawab zada <zada86829@gmail.com>
2025-02-28 23:42:39 +0500

    first commit

--- src/app/components/Sardar.tsx
+++ src/app/components/Sardar.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 const Sardar = () => {
   return (
-<section className='flex flex-col lg:flex-row lg:gap-[190px]'>
+<section className='flex flex-col lg:flex-row '>
 
 
-    <div className='p-3 lg:w-1/2 '>
+    <div className='p-5 lg:w-1/2  '>
       <h1 className='text-2xl font-bold p-3 mt-9 font-serif'>
       Welcome to the Official Website of Sardar Jagmohan Singh
       </h1>
       <div className='bg-red-500 h-1 w-[100px] m-2'></div>
       <h3 className='font-bold p-3 text-2xl font-serif'>A Leader with Vision and Integrity</h3>
-      <p className='font-serif text-xs'>Late Sardar Jagmohan Singh, born on December 8, 1980, was a remarkable individual who left a lasting impact on people from all walks of life. With his warm and soulful nature Sardar Jagmohan Singh touched the hearts of millions worldwide. His legacy of trust and reliability continues to inspire, especially in Canada, where he made significant contributions through his work.</p>
-      <h4 className='text-xl font-bold'>Sardar Jagmohan Singhs Impact in Canada</h4>
-      <p className='font-serif text-xs'>As a Senior Supervisor at Eden Company Sardar Jagmohan Singh played a key role in various projects, particularly in Canada. His leadership in overseeing construction work and managing food packing services helped elevate his reputation as a dedicated and skilled professional. Today, people often search for Sardar Jagmohan Singhs Company in Canada or seek to contact him for professional inquiries, including his WhatsApp number and contact details.</p>
+      <p className='font-serif text-xs lg:text-xl'>Late Sardar Jagmohan Singh, born on December 8, 1980, was a remarkable individual who left a lasting impact on people from all walks of life. With his warm and soulful nature Sardar Jagmohan Singh touched the hearts of millions worldwide. His legacy of trust and reliability continues to inspire, especially in Canada, where he made significant contributions through his work.</p>
+      <h4 className='text-xl font-bold '>Sardar Jagmohan Singhs Impact in Canada</h4>
+      <p className='font-serif text-xs lg:text-xl'>As a Senior Supervisor at Eden Company Sardar Jagmohan Singh played a key role in various projects, particularly in Canada. His leadership in overseeing construction work and managing food packing services helped elevate his reputation as a dedicated and skilled professional. Today, people often search for Sardar Jagmohan Singhs Company in Canada or seek to contact him for professional inquiries, including his WhatsApp number and contact details.</p>
        </div> 
-        <div className=' lg:mt-20 md:mx-auto'><Image src={'/mohamensingh.png'} alt='logo' width={300} height={300}/></div>
+        <div className=' lg:mt-[200px] lg:mx-auto '><Image src={'/mohamensingh.png'} alt='logo' width={350} height={300}/></div>
 </section>
   )
 }
