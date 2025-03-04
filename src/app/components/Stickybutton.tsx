6c45a375721ba51e4b543031131dba1ddfb60e67
nawab zada <zada86829@gmail.com>
2025-03-04 19:13:02 +0500

    updated

--- src/app/components/Stickybutton.tsx
+++ src/app/components/Stickybutton.tsx
@@ -12,7 +12,7 @@ export default function StickyApplyButton() {
     <div className=" font-serif fixed right-4 bottom-20 md:bottom-20 lg:bottom-24 z-50">
       <button
         onClick={handleApplyClick}
-        className="bg-blue-600 text-black px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-white"
+        className="bg-red-600 text-black px-6 py-3 rounded-lg font-semibold shadow-md hover:bg-white"
       >
         Apply now
       </button>
